f978b3f3c6233ed9cc98980066703849d4972bed
mohamed Ibrahim <muhmds@hotmail.com>
2020-01-09 18:15:10 -0500

    update

--- script.js
+++ script.js
@@ -6,21 +6,20 @@ function generate () {
  var complexity = [""]
   
   //possible Password Values
-  var Values = ["!","@","#","$","%","&"]
+  var Values ="ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 
 
   var password = ""
-
+  
   //create for loop to choose password Characters
-  for(var i =0; i <=complexity;i++){
-      password = password + value.charAt(Math.random() * Math.floor(Values.length -1));
-  }
+  for(var i = 0; i <= complexity; i++){
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+}
 //add password to textbox/ Your Secured Password section
 document.getElementById("display").Value = password;
 
 //add password to previously generated passwords section
-document.getElementById("lastNums").innerHTML+=password +"<br />";
-}
+document.getElementById("display").value = password;
 
 function generatePassword() {
     console.log('generate random password');
