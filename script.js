66c7ef7e7c73da86e68aca118a5da69397458c46
mohamed Ibrahim <muhmds@hotmail.com>
2020-04-19 22:38:49 -0400

    updated

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
-// generate Password
-// function generate () {
+function getRandomPassword() {
+    var bigData =[];
+    var passwordPrompt = prompt("Please choose a number between 8 and 128 to determine the amount of characters in your password");
+    
+    var numberOfTimes = (passwordPrompt);
+    console.log()
+    if (numberOftimes < 8 || numberOfTimes > 128) {
+        alert("Please choose a number between 8 and 128")
+    
+    }
+    else {
+        var password = "";
+        var specialCharacters = confirm("would you like special character in your password?");
+        if (specialCharacters === true) {
 
-//     //set password length/complexity
-// } 
- var complexity = [];
-  
-  //possible Password Values
-  var values ="ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+            bigArray.push("@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", ".");
+        }
+    }
+    var numCharYes = confirm("Would you like numbers in your password?");
+    if (numCharYes === true) {
+    bigArray.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+    }
+    var lowerCharYes = confirm("Would you like lower case letters in your password?");
+    if (lowerCharYes === true) {
+    bigArray.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+    }
+    var upperCharYes = confirm("Would you like upper case letters in your password?");
+    if (upperCharYes === true) {
+    bigArray.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+    }
 
+    // this is just to see if I have all the items in the bigArray
+    console.log(bigArray);
 
-  var password = "hello";
-  
- 
-//add password to textbox/ Your Secured Password section
-console.log(password);
-// console.log(document.querySelector("#display"));
-var element = document.getElementById("display")
-console.log(element);
-element.innerText = password;
+    //add password to previously generated passwords section
+document.getElementById("display").value = "Your Secured Password";
 
+var genButton = document.getElementById("generatepass")
+genButton.addEventListener("click", function () {
+    generatePassword()
+
+})
 
 //add password to previously generated passwords section
 document.getElementById("display").value = password;
@@ -27,16 +48,17 @@ document.getElementById("display").value = password;
 // look up addeventlistner
 
 var genButton = document.getElementById("generatepass")
-genButton.addEventListener("click", function(){
+genButton.addEventListener("click", function () {
     generatePassword()
+    console.log()
 })
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-  }
 
+}
 function generatePassword() {
     // get values .length
     // generate random # between 0 and values.length
@@ -44,16 +66,19 @@ function generatePassword() {
     var length = values.length
     var password = ""
     console.log('generate random password');
-     //create for loop to choose password Characters
-    for(var i = 0; i <= 10; i++){
+    //create for loop to choose password Characters
+    for (var i = 0; i <= 10; i++) {
         var rand = getRandomInt(0, length)
         // password += values[rand]
-         password = password + values[rand]
+        password = password + values[rand]
     }
     console.log(password)
-
+    var display = document.getElementById("display")
+    display.innerHTML = password
 }
 
 function copytoclipboard() {
     console.log('copy to clipboard');
 }
+}
+
